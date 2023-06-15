5bd977198b9affc29eb8143c6ca580f0e910768f
Javi Allosa <106932015+javigoall@users.noreply.github.com>
2023-06-15 10:14:20 -0300

    Create saldo_cuenta.py
    Función que muestra y recupera el saldo actual del usuario

---
+++ saldo_cuenta.py
@@ -0,0 +1,27 @@
+def mostrar_saldo(usuario): #La función muestra el saldo actual del usuario pasado como argumento
+ print("Saldo actual: $", usuario["saldo"])
+
+while True:
+ print("Bienvenido")
+ print("1. Registrar usuario")
+ print("2. Iniciar sesión")
+ print("3. Mostrar saldo")
+ print("4. Salir")
+
+ opcion = input("Seleccione una opción: ")
+  
+ if opcion == "1":
+ registrar_usuario()
+ elif opcion == "2":
+ usuario_actual = iniciar_sesion()
+ if usuario_actual is None:
+ continue
+ elif opcion == "3":
+ if "usuario_actual" in locals():
+ mostrar_saldo(usuario_actual)
+ else:
+ print("Debe iniciar sesión para ver el saldo.\n")
+ elif opcion == "4":
+ break
+ else:
+ print("Opción inválida. Por favor, intente nuevamente.\n")
