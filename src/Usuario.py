6d0f7447a37e32da61b7e392822a7348bfaa7f87
Javi Allosa <106932015+javigoall@users.noreply.github.com>
2023-06-26 23:13:31 -0300

    Update Usuario.py
    Modificacion

--- src/Usuario.py
+++ src/Usuario.py
@@ -1,6 +1,7 @@
 class Usuario:
-    def __init__(self, nombre, apellido, email, password):
-        self.nombre = nombre
-        self.apellido = apellido
+    def __init__(self, user, email, password, account, balance):
+        self.user = user
         self.email = email
         self.password = password
+        self.account = account
+        self.balance = balance
