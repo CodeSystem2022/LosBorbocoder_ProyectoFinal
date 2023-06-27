515a31ec08486e217ee40072c92e829c0337cb74
Augusto Britos <augustobritos95@gmail.com>
2023-06-26 21:59:33 -0300

    Avance grupal

---
+++ src/Autenticacion.py
@@ -0,0 +1,22 @@
+from LosBorbocoders_ProyectoFinal.src.base_de_datos.Conexion import Conexion
+
+
+class Autenticacion:
+    def __init__(self, menu):
+        self.menu = menu
+
+    def iniciar_sesion(self):
+        print("Inicio de sesión")
+        nombre = input("Ingrese su nombre de usuario: ")
+        password = input("Ingrese su contraseña: ")
+
+        with Conexion.obtener_conexion() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT COUNT(*) FROM usuarios WHERE nombre = %s AND password = %s", (nombre, password))
+                count = cursor.fetchone()[0]
+
+                if count > 0:
+                    self.menu.usuario_actual = nombre
+                    print("Inicio de sesión exitoso.\n")
+                else:
+                    print("Credenciales incorrectas. Por favor, intente nuevamente.\n")
