2a0892bb0e8ffcff94d55c0ddf7bd35781551be4
Matías Didzius <matiasdidzius1999@gmail.com>
2023-06-27 01:40:57 -0300

    Subo el metodo depositar
    Falta acomodar para que sea dinamico el insertado en la tabla, está hardcodeado pero funciona

--- src/Depositar.py
+++ src/Depositar.py
@@ -1,4 +1,42 @@
-from base_de_datos.Conexion import Conexion
+import psycopg2
+from decimal import Decimal
 
-def depositar():
-    pass
+class Depositar:
+
+    def depositar(self):
+        # Conectarse a la base de datos PostgreSQL
+        conexion = psycopg2.connect(
+            host='127.0.0.1',
+            port='5432',
+            database='test_bd',
+            user='postgres',
+            password='admin'
+        )
+
+        # Crear un cursor
+        cursor = conexion.cursor()
+
+        # Solicitar el monto a depositar al usuario
+        monto = float(input("Ingrese el monto a depositar: "))
+
+        # Obtener el saldo actual de la base de datos
+        cursor.execute("SELECT balance FROM usuarios WHERE account = 123456789")
+        saldo_actual = Decimal(cursor.fetchone()[0])
+        monto_decimal = Decimal(monto)
+
+        # Calcular el nuevo saldo sumando el monto depositado
+        nuevo_saldo = saldo_actual + monto_decimal
+
+        # Actualizar el saldo en la base de datos
+        cursor.execute("UPDATE usuarios SET balance = %s WHERE account = 123456789", (nuevo_saldo,))
+
+        # Confirmar los cambios en la base de datos
+        conexion.commit()
+
+        print("Depósito exitoso. Saldo actualizado.")
+
+        # Cerrar la conexión
+        conexion.close()
+if __name__ == '__main__':
+    depositar = Depositar()
+    depositar.depositar()
