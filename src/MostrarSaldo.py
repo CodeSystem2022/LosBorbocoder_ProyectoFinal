3aaca582d59bf3985bb87591fc94be4b883a8bac
Axel Fornerod <fornerodaxel@hotmail.com>
2023-06-28 00:38:53 -0300

    Agregado de opciones. (Temporal)

--- src/MostrarSaldo.py
+++ src/MostrarSaldo.py
@@ -1,38 +1,76 @@
+from base_de_datos.Conexion import Conexion
+from src import RegistrarUsuario
+from src.Autenticacion import Autenticacion
 
-import psycopg2
-from psycopg2 import connect
 
 class MostrarSaldo:
-    def mostrarSaldo(user_id):
-        conexion = psycopg2.connect(
-            host='127.0.0.1',
-            port='5432',
-            database='test_bd',
-            user='postgres',
-            password='admin'
-        )
+    def mostrarSaldo(self, usuario_actual):
+        conexion = Conexion.obtener_conexion()
 
         # Abro cursor
         cursor = conexion.cursor()
 
+
         # Consulta para obtener el saldo del usuario
-        consulta = "SELECT balance FROM usuarios WHERE account = 123456789"
-        cursor.execute(consulta)
+        consulta = ('SELECT balance FROM usuarios WHERE "user" = %s')
+        cursor.execute(consulta, (usuario_actual,))
+
 
         # Obtener el resultado de la consulta
         resultado = cursor.fetchone()
 
         if resultado:
             saldo = resultado[0]
-            print(f"El saldo del usuario es: {saldo}")
+            while True:
+                print(f"El saldo del usuario es: {saldo}")
+                print("--------------------------")
+                print('1 - Volver al menu principal')
+                print('2 - Cerrar Sesion')
+                valor = input('Seleccione una opcion: ')
+                if valor == "2":
+                    print('Cerrando sesion...')
+                    break
+                elif valor == '1':
+                        print("3. Mostrar saldos")
+                        print("4. Depositar")
+                        print("5. Retirar")
+
+                        print("6. Salir")
+                        opcion = input("Seleccione una opción: ")
+
+                        if opcion == "1":
+                            RegistrarUsuario.registrar_usuario()
+                        elif opcion == "2":
+                            pass
+                            autenticacion = Autenticacion(self)
+                            autenticacion.iniciar_sesion()
+                        elif opcion == "3" and self.usuario_actual:
+                            pass
+                            MostrarSaldo.mostrarSaldo(self, self.usuario_actual)
+                        elif opcion == "4" and self.usuario_actual:
+                            pass
+                            # dep.depositar()
+                        elif opcion == "5" and self.usuario_actual:
+                            pass
+                            # ret.retirar()
+                        elif opcion == "6":
+                            print("Saliendo del programa...")
+                            break
+                        else:
+                            print("Opción inválida. Por favor, intente nuevamente.\n")
+
+                else:
+                    print("Opción inválida. Por favor, intente nuevamente.\n")
         else:
             print("Usuario no encontrado")
+
 
         cursor.close()
         conexion.commit()
         conexion.close()
 
+
+
 if __name__ == '__main__':
     mostrar = MostrarSaldo()
-    mostrar.mostrarSaldo()
-
+    mostrar.mostrarSaldo(usuario_actual)
