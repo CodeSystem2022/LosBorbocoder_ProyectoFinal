d37f5044203a9d52e9c57501aa26d2e3010411fa
Emilse Zalazar <emilsezalazar@gmail.com>
2023-07-02 22:15:04 -0300

    limpiando codigo

--- src/base_de_datos/Conexion.py
+++ src/base_de_datos/Conexion.py
@@ -31,29 +31,3 @@ class Conexion:
     def obtener_cursor(cls, conexion):
         return conexion.cursor()
 
-    @classmethod
-    def obtener_pool(cls):
-        if cls._pool is None:
-            try:
-                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
-                                                      cls._MAX_CON,
-                                                      host=cls._HOST,
-                                                      user=cls._USERNAME,
-                                                      password=cls._PASSWORD,
-                                                      port=cls._DB_PORT,
-                                                      database=cls._DATABASE)
-
-                return cls._pool
-            except Exception as e:
-                print(f"Ha ocurrido un error: {e}")
-
-                sys.exit()
-        else:
-            return cls._pool
-
-if __name__ == '__main__':
-    conexion1 = Conexion.obtener_conexion()
-    conexion2 = Conexion.obtener_conexion()
-    conexion3 = Conexion.obtener_conexion()
-    conexion4 = Conexion.obtener_conexion()
-    conexion5 = Conexion.obtener_conexion()
